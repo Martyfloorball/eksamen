042d7cc5749db12a2f3ba899c878b5af2c40b988
capttwigg <mike@familien-jahn.dk>
2018-04-30 13:07:48 +0200

    Initial commit

---
+++ src/main/java/com/eksamen/eksamen/EksamenApplication.java
@@ -0,0 +1,12 @@
+package com.eksamen.eksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamenApplication.class, args);
+    }
+}
