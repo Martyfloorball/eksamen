922eee885acbf1af1022560a9b0d77ce19857859
Valhalla <martinfloorball1997@gmail.com>
2018-05-04 11:40:16 +0200

    Tilføjet SessionController

--- src/main/java/com/eksamen/eksamen/Controller/SessionController.java
+++ src/main/java/com/eksamen/eksamen/Controller/SessionController.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
-
-    @GetMapping("/")
-    public String index() {
-        return "/index";
-    }
+public class SessionController {
+  @GetMapping("/login")
+  public String login() {
+    return "login";
+  }
 }
