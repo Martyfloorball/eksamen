f5506ea1c0571eb3d5b2ae0c0f5a64fa1e1624b4
Marco Romeri <marco@romeri.dk>
2018-05-06 22:58:41 +0200

    Dashboard and some menu

--- src/main/java/com/eksamen/eksamen/Controller/SessionController.java
+++ src/main/java/com/eksamen/eksamen/Controller/SessionController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SessionController {
-  @GetMapping("/login")
-  public String login() {
+    @GetMapping("/login")
+    public String login() {
     return "login";
   }
 }
