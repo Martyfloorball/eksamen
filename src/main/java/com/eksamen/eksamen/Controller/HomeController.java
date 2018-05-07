6c49d8d1d817d42274be94887a9049620046a6b8
Marco Romeri <marco@romeri.dk>
2018-05-07 15:42:26 +0200

    Menues

--- src/main/java/com/eksamen/eksamen/Controller/HomeController.java
+++ src/main/java/com/eksamen/eksamen/Controller/HomeController.java
@@ -1,9 +1,7 @@
 package com.eksamen.eksamen.Controller;
 
-import com.eksamen.eksamen.Handler.DatabaseHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.sql.ResultSet;
 
 @Controller
 public class HomeController {
