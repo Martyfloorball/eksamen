01bc708e037b4941fb9540d757e07c2fdd420c57
JanniJensenDahm <tjukki1994@hotmail.com>
2018-05-07 10:55:56 +0200

    added getter + setter, isLoggedIn() and fields in session

--- src/main/java/com/eksamen/eksamen/Base/Session.java
+++ src/main/java/com/eksamen/eksamen/Base/Session.java
@@ -1,4 +1,29 @@
 package com.eksamen.eksamen.Base;
 
+import com.eksamen.eksamen.Handler.DatabaseHandler;
+
 public class Session {
+    private static int id = 0;
+    private static int userniveau;
+
+    //Hvis id ikke er 0 returneres true, og brugeren bliver logget ind
+    public static boolean isLoggedIn(){
+        return id != 0;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        Session.id = id;
+    }
+
+    public static int getUserniveau() {
+        return userniveau;
+    }
+
+    public static void setUserniveau(int userniveau) {
+        Session.userniveau = userniveau;
+    }
 }
