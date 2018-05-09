7e8a60d1cadbb0feff780d1e3944d53a1f0c5273
Marco Romeri <marco@romeri.dk>
2018-05-09 14:39:25 +0200

    HomeController addition

--- src/main/java/com/eksamen/eksamen/Controller/HomeController.java
+++ src/main/java/com/eksamen/eksamen/Controller/HomeController.java
@@ -1,9 +1,16 @@
 package com.eksamen.eksamen.Controller;
 
+import com.eksamen.eksamen.Base.Session;
+import com.eksamen.eksamen.Base.Shift;
+import com.eksamen.eksamen.Handler.DatabaseHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 
 @Controller
@@ -13,9 +20,9 @@ public class HomeController {
         model.addAttribute("weekNames", getWeekNames());
         model.addAttribute("days", getDays());
         model.addAttribute("months", getMonths());
+        model.addAttribute("myShifts", getMyShifts());
         return "/index";
     }
-
 
     private String[] getWeekNames() {
         Calendar calendar = Calendar.getInstance();
@@ -56,5 +63,58 @@ public class HomeController {
         }
 
         return returnMonths;
+    }
+
+    private ArrayList<Shift>[] getMyShifts() {
+        Calendar calendar = Calendar.getInstance();
+
+        String start = String.valueOf(calendar.get(Calendar.YEAR)+"-"+(calendar.get(Calendar.MONTH)+1)+"-"+calendar.get(Calendar.DAY_OF_MONTH)+" 00:00:00");
+        calendar.add(Calendar.DATE, 7);
+        String end = String.valueOf(calendar.get(Calendar.YEAR)+"-"+(calendar.get(Calendar.MONTH)+1)+"-"+calendar.get(Calendar.DAY_OF_MONTH)+" 23:59:59");
+        calendar.add(Calendar.DATE, -7);
+
+        ArrayList<Shift>[] returnMyShifts = (ArrayList<Shift>[])new ArrayList[7];
+
+        String sql = "SELECT shift_id,comment,start_time,end_time,location_name,firstname,lastname,staff_id,approved_by " +
+                "FROM shift " +
+                "INNER JOIN shift_request ON fk_shift_id " +
+                "INNER JOIN location ON location_id = fk_location_id " +
+                "INNER JOIN staff ON staff_id = fk_staff_id " +
+                "WHERE staff_id = '"+ Session.getId()+"' " +
+                "AND start_time BETWEEN '"+start+"' AND '"+end+"'";
+        System.out.println(sql);
+        ResultSet resultSet = DatabaseHandler.getInstance().querySelect(sql);
+
+        for(int i = 0; i < returnMyShifts.length; i++) {
+            returnMyShifts[i] = new ArrayList<Shift>();
+            try {
+                while(resultSet.next()) {
+                    if(String.valueOf(resultSet.getDate("start_time")).equals(String.valueOf(calendar.get(Calendar.YEAR)+"-"+(calendar.get(Calendar.MONTH)+1 < 10 ? "0"+(calendar.get(Calendar.MONTH)+1) : calendar.get(Calendar.MONTH)+1)+"-"+(calendar.get(Calendar.DAY_OF_MONTH) < 10 ? "0"+calendar.get(Calendar.DAY_OF_MONTH) : calendar.get(Calendar.DAY_OF_MONTH)))))  {
+                        returnMyShifts[i].add(new Shift(
+                                resultSet.getInt("shift_id"),
+                                resultSet.getString("comment"),
+                                resultSet.getInt("approved_by"),
+                                String.valueOf(resultSet.getDate("start_time")),
+                                String.valueOf(resultSet.getTime("start_time")),
+                                String.valueOf(resultSet.getDate("end_time")),
+                                String.valueOf(resultSet.getTime("end_time")),
+                                resultSet.getString("location_name"),
+                                resultSet.getInt("staff_id"),
+                                resultSet.getString("firstname")+" "+resultSet.getString("lastname")));
+                    }
+                    System.out.println();
+                }
+
+                calendar.add(Calendar.DATE, 1);
+                resultSet.first();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(Arrays.toString(returnMyShifts));
+
+        return returnMyShifts;
     }
 }
