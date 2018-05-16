be24ec8cf5cb47859b0838f03a4dac625efb87c1
Marco Romeri <marco@romeri.dk>
2018-05-16 13:52:53 +0200

    Super cons on worker, admin and leader

--- src/main/java/com/eksamen/eksamen/Base/Worker.java
+++ src/main/java/com/eksamen/eksamen/Base/Worker.java
@@ -1,8 +1,7 @@
 package com.eksamen.eksamen.Base;
 
 public class Worker extends Staff{
-  @Override
-  public int getStaffNiveau() {
-    return super.getStaffNiveau() + 3;
+  public Worker(int staffId, String firstName, String lastName, String email, String password, int phonenumber, int staffNiveau) {
+    super(staffId, firstName, lastName, email, password, phonenumber, staffNiveau);
   }
 }
